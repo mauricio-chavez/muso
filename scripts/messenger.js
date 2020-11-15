8676b9e4bf86179e1f43621ad43fa9ee25403536
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-11-15 04:31:03 -0600

    Multiurl support for whitelist

--- scripts/messenger.js
+++ scripts/messenger.js
@@ -3,12 +3,17 @@ const axios = require('axios');
 const action = process.argv[2];
 switch (action) {
   case 'whitelist':
+    const domains = [];
+    for (let i = 2; i < process.argv.length; i++) {
+      const domain = process.argv[i];
+      domains.push(push);
+    }
     const url = process.argv[3];
     axios
       .post(
         'https://graph.facebook.com/v9.0/me/messenger_profile',
         {
-          whitelisted_domains: [url],
+          whitelisted_domains: domains,
         },
         {
           params: {
